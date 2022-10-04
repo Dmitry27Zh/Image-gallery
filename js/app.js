18ecd3344170f432c9b7bbfa28fac41cdbf719c3
Dmitry <dmitry27zh@gmail.com>
2022-10-04 17:59:54 +0500

    Add popup

--- js/app.js
+++ js/app.js
@@ -1,24 +1,63 @@
 const imagesCount = 15
 const galleryElement = document.getElementById('gallery')
+const popupElement = document.getElementById('popup')
 
-const createImage = (src) => {
+const createImage = (src, classList, focusable) => {
   const element = document.createElement('img')
-  element.classList.add('gallery-img')
+  element.classList.add(...classList)
   element.src = src
-  element.setAttribute('tabindex', '0')
+
+  if (focusable) {
+    element.setAttribute('tabindex', '0')
+  }
+
   return element
 }
 
-const renderImage = (index) => {
-  const path = `./img/architecture-${index}.jpg`
-  const imageElement = createImage(path)
-  galleryElement.append(imageElement)
+const renderImage = (src, classList, container, onImageElementAction) => {
+  const imageElement = createImage(src, classList, true)
+  container.append(imageElement)
+
+  if (typeof onImageElementAction === 'function') {
+    onImageElementAction(imageElement)
+  }
+}
+
+const openPopup = (src) => {
+  renderImage(src, ['popup-img'], popupElement)
+  popupElement.classList.add('is-shown')
+}
+
+const closePopup = () => {
+  if (popupElement.classList.contains('is-shown')) {
+    popupElement.classList.remove('is-shown')
+    popupElement.innerHTML = ''
+  }
+}
+
+const addPopupListeners = () => {
+  popupElement.addEventListener('click', () => {
+    closePopup()
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      closePopup()
+    }
+  })
 }
 
 const renderImages = () => {
-  for (let i = 1; i < imagesCount; i++) {
-    renderImage(i)
+  for (let i = 1; i <= imagesCount; i++) {
+    const src = `./img/architecture-${i}.jpg`
+
+    renderImage(src, ['gallery-img'], galleryElement, (imageElement) => {
+      imageElement.addEventListener('click', () => {
+        openPopup(src)
+      })
+    })
   }
 }
 
 renderImages()
+addPopupListeners()
